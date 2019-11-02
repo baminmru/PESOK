90b97222a81fdec194d9e445fe20be3d19b70c5b
Michael Baranov <developer.bami@gmail.com>
2019-11-02 11:54:17 +0300

    Update test.cmd

--- test.cmd
+++ test.cmd
@@ -2,5 +2,5 @@ del *.svg
 rem each layer in new file
 .\Slic3r-master_changed\build\debug\slic3r.exe  .\voronka.stl .\iy.stl --load .\config.ini --layer-svg --output test
 rem all layers in single file
-.\Slic3r-master_changed\build\debug\slic3r.exe  .\voronka.stl d:\bami\projects\old\PESOK\iy.stl --load .\config.ini --export-svg --output s_test.svg
+.\Slic3r-master_changed\build\debug\slic3r.exe  .\voronka.stl .\iy.stl --load .\config.ini --export-svg --output s_test.svg
 pause
